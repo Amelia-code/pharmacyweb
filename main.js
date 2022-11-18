39d058d9bb675d18d6cfd5b26ee4376fd91e76e2
soyoung <60204446+ysy3012000@users.noreply.github.com>
2022-11-18 16:39:37 +0900

    me

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
-    mapOption = {
-        center: new kakao.maps.LatLng(37.5170977988592, 127.04704243875862), // 지도의 중심좌표
-        level: 4 // 지도의 확대 레벨
-    };
+var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
+    center: new kakao.maps.LatLng(36.2683, 127.6358), // 지도의 중심좌표 
+    level: 14 // 지도의 확대 레벨 
+});
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 function locationLoadSuccess(pos) {
